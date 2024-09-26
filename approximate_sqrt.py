ea974a14a1d038829a764155a3b6b4b1461da3c4
Toni Silfver <71206346+tosilfv@users.noreply.github.com>
2024-09-26 13:30:20 +0300

    Update approximate_sqrt.py

--- approximate_sqrt.py
+++ approximate_sqrt.py
@@ -63,15 +63,61 @@ square_root(2)
 """
 
 ## Evolution D
+#from doctest import run_docstring_examples
+
+#def approx(x, a):
+#    """Approximates the square root of a, returns float.
+    
+#    >>> approx(2, 4)
+#    2.0
+#    """
+#    return 0.5 * (x + a / x)
+
+#def close(x, y, tolerance = 1e-14):
+#    """Compares absolute value of x - y and tolerance, returns bool.
+    
+#    >>> close(1 * 1, 25)
+#    False
+#    """
+#    assert(tolerance > 1e-15), 'tolerance must be over 1e-15'
+#    return abs(x - y) < tolerance
+
+#def calc(approx, close, guess = 1, max_approxs = 100):
+#    """Calculates close approximation of e.g. square root, returns float."""
+#    k = 0
+#    while not close(guess) and k < max_approxs:
+#        guess = approx(guess)
+#        print(f"guess: {guess}")
+#        k = k + 1
+#    return guess
+
+#def square_root(a):
+#    return calc(lambda x: approx(x, a),
+#                   lambda x: close(x * x, a))
+
+#run_docstring_examples(approx, globals(), True)
+#run_docstring_examples(close, globals(), True)
+#### Testing a Huge number:
+#square_root(14537524845386523763451735673455437524.26268675964722634563426432613)
+
+## Evolution E
 from doctest import run_docstring_examples
 
-def approx(x, a):
+def approx_sqrt(x, a):
     """Approximates the square root of a, returns float.
     
-    >>> approx(2, 4)
+    >>> approx_sqrt(2, 4)
     2.0
     """
-    return 0.5 * (x + a / x)
+    return (x + a / x) / 2
+
+def approx_cubert(x, a):
+    """Approximates the cube root of a, returns float.
+    
+    >>> approx_cubert(2, 8)
+    2.0
+    """
+    return (2*x + a / (x*x)) / 3
 
 def close(x, y, tolerance = 1e-14):
     """Compares absolute value of x - y and tolerance, returns bool.
@@ -92,10 +138,17 @@ def calc(approx, close, guess = 1, max_approxs = 100):
     return guess
 
 def square_root(a):
-    return calc(lambda x: approx(x, a),
-                   lambda x: close(x * x, a))
+    return calc(lambda x: approx_sqrt(x, a),
+                   lambda x: close(x*x, a))
 
-run_docstring_examples(approx, globals(), True)
+def cube_root(a):
+    return calc(lambda x: approx_cubert(x, a),
+                   lambda x: close(x*x*x, a))
+
+run_docstring_examples(approx_sqrt, globals(), True)
+run_docstring_examples(approx_cubert, globals(), True)
 run_docstring_examples(close, globals(), True)
 ### Testing a Huge number:
 square_root(14537524845386523763451735673455437524.26268675964722634563426432613)
+print("******************************************")
+cube_root(8)
